a02f14a7a0d41f4f0c60fd7fe19298a83197ace3
pc <maxson10000@yandex.ru>
2024-10-06 14:27:12 +0300

    lession2

---
+++ 2/lesson2.py
@@ -0,0 +1,22 @@
+#def my_sum(a,b):
+#    return a+b
+#print (my_sum(a:5 , b:6))
+
+def get_chetnoe(chislo):
+    if chislo // 2 == 0:
+        print ('Чётное')
+    else:
+        print ('нечётное')
+
+get_chetnoe(31)
+
+
+#def say_hello(name):
+#    print("Привет", name)
+
+#    say_hello("Pavel")
+#    say_hello("Alexsey")
+#    say_hello("Maksim")
+#[1,2,3,4,5,6,7,8,9]
+#def get_max(my_list)
+ #   l_list = len(my_list)
